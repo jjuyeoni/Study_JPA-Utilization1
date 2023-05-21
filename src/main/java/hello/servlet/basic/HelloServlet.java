ef361b9b08b207e431e7c51ddbc04841fec8e8c3
jjuyeoni <cwa302@naver.com>
2023-05-22 01:58:12 +0900

    second commit

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,8 +1,13 @@
-# SpringBoot
-### 스프링 부트 공부 기록
-https://velog.io/@jjuyeoni/%EC%84%9C%EB%B8%94%EB%A6%BF
-- hello 서블릿 호출
-```
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
@@ -11,7 +16,7 @@ public class HelloServlet extends HttpServlet {
         System.out.println("HelloServlet.service");
         System.out.println("req = " + req);
         System.out.println("resp = " + resp);
-        
+
         String username = req.getParameter("username"); // 쿼리 파라미터 조회
         System.out.println("username = " + username);
 
@@ -21,15 +26,3 @@ public class HelloServlet extends HttpServlet {
 
     }
 }
-```
-- HttpServletRequest
-  - 기본 사용법, Header 조회
-  - HTTP 요청 메시지 바디 조회
-    - GET -쿼리 파라미터
-    - POST - HTML Form
-    - HTTP API - MessageBody -> Postman 테스트
-- HttpServletResponse
-  - 기본 사용법, Header 조회
-  - HTTP 응답 메시지 바디 조회
-    - HTML 응답
-    - HTTP API JSON 응답
