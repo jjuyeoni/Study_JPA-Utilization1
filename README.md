65793416ecad301bf668ca8c3f5a7262c6e32253
jjuyeoni <cwa302@naver.com>
2023-05-22 21:28:59 +0900

    mvc 패턴까지 완료

--- README.md
+++ README.md
@@ -1,42 +1,28 @@
 # SpringBoot
 ### 스프링 부트 공부 기록
 https://velog.io/@jjuyeoni/%EC%84%9C%EB%B8%94%EB%A6%BF
+
+<br> 
+index.html 파일 참고<br>
+
 - hello 서블릿 호출
-```
-@WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet extends HttpServlet {
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        System.out.println("HelloServlet.service");
-        System.out.println("req = " + req);
-        System.out.println("resp = " + resp);
-        
-        String username = req.getParameter("username"); // 쿼리 파라미터 조회
-        System.out.println("username = " + username);
-
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().write("hello " + username);
-
-    }
-}
-```
 - HttpServletRequest
   - 기본 사용법, Header 조회
   - HTTP 요청 메시지 바디 조회
     - GET -쿼리 파라미터
-      <br>복수 파라미터에서 단일 파라미터 조회<br>
-      request.getParameter() 는 하나의 파라미터 이름에 대해서 단 하나의 값만 있을 때 사용해야 한다.
-      <br> if. 중복일 때는 request.getParameterValues() 를 사용해야 한다.
-      <br> 참고로 이렇게 중복일 때 request.getParameter() 를 사용하면 request.getParameterValues() 의
-      첫 번째 값을 반환한다<br>
-      <br>
     - POST - HTML Form
     - HTTP API - MessageBody -> Postman 테스트
     <br> http 요청 데이터에 message body 에 내가 원하는 데이터를 직접 담아서 요청
 - HttpServletResponse
   - 기본 사용법, Header 조회
   - HTTP 응답 메시지 바디 조회
+    - 단순 텍스트 응답(생략)
     - HTML 응답
     - HTTP API JSON 응답
+- 서블릿/jsp으로 회원 관리 웹 애플리케이션 만들기
+- MVC 패턴 - 개요/적용/한계
+- MVC 프레임워크 만들기
+  - 프론트 컨트롤러 도입
+  - view 분리, Model 추가
+  - 단순하고 실용적인 컨트롤러
+  - 유연한 컨트롤러
---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,70 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //[status-line]
+        resp.setStatus(HttpServletResponse.SC_OK); // 200
+
+        //[response-header]
+        resp.setHeader("Content-Type", "text/plain");
+        // 과거 버전 캐시 지우기 - 여기부터
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cahe");
+        // 과거 버전 캐시 지우기 - 여기까지
+
+        // customizing header
+        resp.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드]
+        content(resp);
+        cookie(resp);
+        redirect(resp);
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("ok");
+
+    }
+
+    // [content 편의 메소드]
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    // [쿠키 편의 메소드]
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    // [redirect 편의 메소드]
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+//        response.setStatus(HttpServletResponse.SC_FOUND); //302
+//        response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+
+    }
+}
